cacaba6f092a078bc55debaaece8f6b3cd58f368
Dermofet <71003028+Dermofet@users.noreply.github.com>
2023-02-21 23:01:42 +0300

    Delete FastAPI_SQLAlchemy directory

--- FastAPI_SQLAlchemy/_fastapi_/response_detail.py
+++
@@ -1,3 +0,0 @@
-unexpected_parameters_406 = "Request parameters are not correct. Maybe you forgot to fill the database"
-already_exist_499 = "Object already exists"
-server_error_500 = "Internal Server Error"
